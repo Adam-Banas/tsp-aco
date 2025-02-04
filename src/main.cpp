a4e4c00ba2d9ad716fdf86b600a1696bd59d3d39
Adam Banas <adam.banas.1989@gmail.com>
2025-02-04 14:54:19 +0100

    Add benchmarks
    Current results for the following setup:
    Nodes: 64
    Agents: 512
    
    Algorithm: CPU
    Total time: 12418 ms
    Best iteration: 592 ms
    Worst iteration: 631 ms
    
    Algorithm: GPU
    Total time: 11980 ms
    Best iteration: 585 ms
    Worst iteration: 611 ms

--- src/main.cpp
+++ src/main.cpp
@@ -43,34 +43,8 @@ std::ostream& operator<<(std::ostream& out, const std::vector<aco::Graph::Index>
     return out;
 }
 
-int main(int argc, char* argv[]) {
-    // Parse command line arguments
-    if (argc != 2) {
-        std::cout
-            << "Ant Colony Optimization algorithm applied to the Travelling Salesman Problem.\n";
-        std::cout << "Usage: " << argv[0] << " iterations\n";
-        return 1;
-    }
-    int max_iterations = std::stoi(argv[1]);
-
-    // Configuration
-    std::size_t cities = 64;
-    std::size_t agents = cities * 16;
-    float       pheromone_evaporation = 0.9; // 10% of pheromone evaporates every iteration
-    float       min_pheromone = 0.1; // Minimum pheromone on an edge (which is also the starting
-                                     // pheromone). I observed the following rough guidelines:
-    // - The greater total distance to travel (depends on the number of cities and the distances
-    //   between them), the smaller this number needs to be.
-    // - The more ants we have, the bigger this number can be.
-
-    // Initialization
-    std::random_device rd;
-    std::mt19937       gen(rd());
-    aco::Graph         graph = aco::Graph(gen, cities, min_pheromone);
-
-    aco::Algorithm::Config config = {.agents_count = agents,
-                                     .pheromone_evaporation = pheromone_evaporation};
-
+void standard_simulation(int max_iterations, std::mt19937& gen, aco::Graph graph,
+                         aco::Algorithm::Config config) {
     std::vector<std::unique_ptr<aco::Algorithm>> algorithms;
     algorithms.push_back(aco::Algorithm::make(aco::DeviceType::CPU, gen, graph, config));
     algorithms.push_back(aco::Algorithm::make(aco::DeviceType::CPU, gen, graph, config));
@@ -94,5 +68,97 @@ int main(int argc, char* argv[]) {
             std::cout << "Previous best: " << previous_best
                       << ", length: " << algorithm->path_length(previous_best) << "\n";
         }
+    }
+}
+
+void benchmark_simulation(int max_iterations, std::mt19937& gen, aco::Graph graph,
+                          aco::Algorithm::Config config) {
+    std::vector<std::unique_ptr<aco::Algorithm>> algorithms;
+    algorithms.push_back(aco::Algorithm::make(aco::DeviceType::CPU, gen, graph, config));
+    algorithms.push_back(aco::Algorithm::make(aco::DeviceType::GPU, gen, graph, config));
+
+    struct Result {
+        std::string      info;
+        int              best_path_length;
+        std::vector<int> iteration_times;
+        int              total_time;
+    };
+    std::vector<Result> results;
+
+    // Main loop
+    for (auto& algorithm : algorithms) {
+        Result algorithm_result;
+        std::cout << "Starting simulation using algorithm: " << algorithm->info() << "...\n";
+        algorithm_result.iteration_times.resize(max_iterations);
+        auto begin = std::chrono::steady_clock::now();
+        for (int i = 0; i < max_iterations; ++i) {
+            auto iter_begin = std::chrono::steady_clock::now();
+            auto iteration_best = algorithm->advance();
+            auto iter_end = std::chrono::steady_clock::now();
+            algorithm_result.iteration_times[i] =
+                std::chrono::duration_cast<std::chrono::milliseconds>(iter_end - iter_begin)
+                    .count();
+            std::cout << "Iteration time: " << algorithm_result.iteration_times[i]
+                      << " ms, path length: " << algorithm->path_length(iteration_best) << "\n";
+        }
+        // TODO: Synchronize
+        auto end = std::chrono::steady_clock::now();
+        algorithm_result.total_time =
+            std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+        algorithm_result.info = algorithm->info();
+        algorithm_result.best_path_length = algorithm->path_length(algorithm->get_shortest_path());
+
+        std::cout << "Total time: " << algorithm_result.total_time << " ms.\n";
+        results.push_back(std::move(algorithm_result));
+    }
+
+    std::cout << "\n\nAll simulation finished, summary:\n";
+    for (auto elem : results) {
+        std::cout << "Algorithm: " << elem.info << "\n";
+        std::cout << "Final path length: " << elem.best_path_length << "\n";
+        std::cout << "Total time: " << elem.total_time << " ms\n";
+        std::cout << "Best iteration: "
+                  << *std::min_element(begin(elem.iteration_times), end(elem.iteration_times))
+                  << " ms\n";
+        std::cout << "Worst iteration: "
+                  << *std::max_element(begin(elem.iteration_times), end(elem.iteration_times))
+                  << " ms\n";
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // Parse command line arguments
+    if (argc != 2) {
+        std::cout << "Ant Colony Optimization algorithm applied to the Travelling Salesman "
+                     "Problem.\n";
+        std::cout << "Usage: " << argv[0] << " iterations\n";
+        return 1;
+    }
+    int max_iterations = std::stoi(argv[1]);
+
+    // Configuration
+    std::size_t cities = 64;
+    std::size_t agents = cities * 16;
+    float       pheromone_evaporation = 0.9; // 10% of pheromone evaporates every iteration
+    float       min_pheromone = 0.1; // Minimum pheromone on an edge (which is also the starting
+                                     // pheromone). I observed the following rough guidelines:
+    // - The greater total distance to travel (depends on the number of cities and the distances
+    //   between them), the smaller this number needs to be.
+    // - The more ants we have, the bigger this number can be.
+
+    // Initialization
+    std::random_device rd;
+    std::mt19937       gen(rd());
+    aco::Graph         graph = aco::Graph(gen, cities, min_pheromone);
+
+    aco::Algorithm::Config config = {.agents_count = agents,
+                                     .pheromone_evaporation = pheromone_evaporation};
+
+    bool benchmark = true;
+    if (!benchmark) {
+        standard_simulation(max_iterations, gen, graph, config);
+    } else {
+        benchmark_simulation(max_iterations, gen, graph, config);
     }
 }
